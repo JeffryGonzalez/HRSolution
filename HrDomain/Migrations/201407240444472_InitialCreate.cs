4237438b9ea7d7e3dea8756461c58b458d4ca1d6
Jeffry Gonzalez <jeff@hypertheory.com>
2014-07-25 07:07:41 -0600

    added migrations and initial seed

---
+++ HrDomain/Migrations/201407240444472_InitialCreate.cs
@@ -0,0 +1,29 @@
+namespace HrDomain.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Salary = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Email = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Employees");
+        }
+    }
+}
